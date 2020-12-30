17cb194d25db80c3380d7ff9d449c2d6481abbce
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2020-12-30 11:00:37 +0900

    rename

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -5,5 +5,5 @@ import { withScreenshot } from 'storybook-chrome-screenshot';
 
 storiesOf('Button', module)
 	.addDecorator(withScreenshot())
-  .add('with default style', () => <Button>Default</Button>)
-  .add('with primary style', () => <Button primary>Primary</Button>);
+  .add('with-default-style', () => <Button>Default</Button>)
+  .add('with-primary-style', () => <Button primary>Primary</Button>);
